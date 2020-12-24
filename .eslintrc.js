1ca01cfcafb6cec1113eff077b53c1069c03a1fb
yuuangzhang <yuuangzhang@tencent.com>
2020-12-24 13:24:50 +0800

    chore: update rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,14 @@ module.exports = {
   extends: 'standard-with-typescript',
   parserOptions: {
     project: './tsconfig.json'
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/naming-convention': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    '@typescript-eslint/no-dynamic-delete': 'off',
+    '@typescript-eslint/no-misused-promises': 'off'
   }
 }
