025948c54281a09b1203b09dabb67f0b600bc85b
yuuangzhang <yuuangzhang@tencent.com>
2021-02-23 20:51:43 +0800

    chore: add react hooks extends

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: 'standard-with-typescript',
+  extends: ['standard-with-typescript', 'plugin:react-hooks/recommended'],
   parserOptions: {
     project: './tsconfig.json'
   },
