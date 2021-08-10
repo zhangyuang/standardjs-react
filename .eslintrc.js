a5f269216cc28a26f531071ee92740e4b00aa1cc
yuuangzhang <yuuangzhang@tencent.com>
2021-08-10 17:28:40 +0800

    chore: off import/export

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     '@typescript-eslint/no-floating-promises': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-unnecessary-type-assertion': 'off',
-    '@typescript-eslint/consistent-type-assertions': 'off'
+    '@typescript-eslint/consistent-type-assertions': 'off',
+    'import/export': 'off'
   }
 }
